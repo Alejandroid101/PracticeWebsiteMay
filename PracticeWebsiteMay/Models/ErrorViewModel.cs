8a12e2bf966534b63c82ff4ff97c719124d110d0
Alejandroid101 <kik.rdl3@gmail.com>
2020-05-26 16:11:48 -0700

    Add project files.

---
+++ PracticeWebsiteMay/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PracticeWebsiteMay.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
